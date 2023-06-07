6392f9c9bd76a6446350455ced198fe4300482f0
Daniel Saewitz <switz213@gmail.com>
2023-06-07 17:31:46 -0400

    Rename flags to derived; bump v to 0.4.0; move types to independent file

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,6 @@
-type FlagFn<StateKeys extends string> = (
-  states: Record<StateKeys, boolean | undefined>,
-  stateEnums: Record<StateKeys, number>,
-  activeEnum: number | undefined
-) => unknown;
+import type { Config, DerivedConfig, Return } from './types.d.ts';
 
-type Config<T extends string, K extends FlagsConfig<T>> = {
-  states: Record<T, boolean | undefined>;
-  flags: K;
-};
-
-type RequireAtLeastOne<T, Keys extends keyof T = keyof T> = Pick<T, Exclude<keyof T, Keys>> &
-  {
-    [K in Keys]-?: Required<Pick<T, K>> & Partial<Pick<T, Exclude<Keys, K>>>;
-  }[Keys];
-
-type FlagsConfig<T extends string> = Record<
-  string | symbol | number,
-  RequireAtLeastOne<Partial<Record<T, unknown>>, T> | FlagFn<T> | T[]
->;
-
-type Return<T extends string, K extends FlagsConfig<T>> = FlagsReturn<T, K> & {
-  activeState: T | undefined;
-  activeEnum: number | undefined;
-  stateEnums: Record<T, number>;
-  states: Record<T, boolean | undefined>;
-};
-
-type FlagsReturn<StateKeys extends string, K extends FlagsConfig<StateKeys>> = {
-  [P in keyof K]: K[P] extends FlagFn<StateKeys>
-    ? ReturnType<K[P]>
-    : K[P] extends Array<StateKeys>
-    ? boolean
-    : K[P] extends object
-    ? K[P][keyof K[P]]
-    : undefined;
-};
-function driver<const T extends string, K extends FlagsConfig<T>>(
+function driver<const T extends string, K extends DerivedConfig<T>>(
   config: Config<T, K>
 ): Return<T, K> {
   const activeState = Object.keys(config.states).find((key) => !!config.states[key as T]) as T;
@@ -45,21 +10,25 @@ function driver<const T extends string, K extends FlagsConfig<T>>(
   const enums: Record<string, number> = {};
   stateKeys.forEach((key, index) => (enums[key] = index));
 
-  const flags = Object.entries(config.flags).map(([key, value]) => {
-    if (typeof value === 'function') {
-      return [key, value(config.states, enums, activeEnum)];
-    }
+  const derived = config.derived ?? config.flags;
 
-    if (Array.isArray(value)) {
-      return [key, value.includes(activeState)];
-    }
+  const flags = derived
+    ? Object.entries(derived).map(([key, value]) => {
+        if (typeof value === 'function') {
+          return [key, value(config.states, enums, activeEnum)];
+        }
 
-    if (typeof value === 'object') {
-      return [key, value[activeState]];
-    }
+        if (Array.isArray(value)) {
+          return [key, value.includes(activeState)];
+        }
 
-    return [key, value];
-  });
+        if (typeof value === 'object') {
+          return [key, value[activeState]];
+        }
+
+        return [key, value];
+      })
+    : [];
 
   return Object.assign({}, Object.fromEntries(flags), {
     activeState,
---
+++ src/types.d.ts
@@ -0,0 +1,41 @@
+type RequireAtLeastOne<T, Keys extends keyof T = keyof T> = Pick<T, Exclude<keyof T, Keys>> &
+  {
+    [K in Keys]-?: Required<Pick<T, K>> & Partial<Pick<T, Exclude<Keys, K>>>;
+  }[Keys];
+
+type DerivedFn<StateKeys extends string> = (
+  states: Record<StateKeys, boolean | undefined>,
+  stateEnums: Record<StateKeys, number>,
+  activeEnum: number | undefined
+) => unknown;
+
+export type Config<T extends string, K extends DerivedConfig<T>> = {
+  states: Record<T, boolean | undefined>;
+  /**
+   * @deprecated the `flags` field has been renamed to derived, please transition your code
+   */
+  flags?: K;
+  derived?: K;
+};
+
+export type DerivedConfig<T extends string> = Record<
+  string | symbol | number,
+  RequireAtLeastOne<Partial<Record<T, unknown>>, T> | DerivedFn<T> | T[]
+>;
+
+export type Return<T extends string, K extends DerivedConfig<T>> = DerivedReturn<T, K> & {
+  activeState: T | undefined;
+  activeEnum: number | undefined;
+  stateEnums: Record<T, number>;
+  states: Record<T, boolean | undefined>;
+};
+
+type DerivedReturn<StateKeys extends string, K extends DerivedConfig<StateKeys>> = {
+  [P in keyof K]: K[P] extends DerivedFn<StateKeys>
+    ? ReturnType<K[P]>
+    : K[P] extends Array<StateKeys>
+    ? boolean
+    : K[P] extends object
+    ? K[P][keyof K[P]]
+    : undefined;
+};
