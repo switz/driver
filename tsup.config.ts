910794cd97baac2db411a9a5bd43ba68b5b0545e
Daniel Saewitz <switz213@gmail.com>
2023-06-19 15:50:52 -0400

    Add default esbuild options

--- tsup.config.ts
+++ tsup.config.ts
@@ -21,5 +21,14 @@ export default defineConfig((options) => [
     format: 'cjs',
     outDir: './dist/cjs/',
     outExtension: () => ({ js: '.cjs' }),
+    esbuildOptions: (options) => {
+      options.footer = {
+        // This will ensure we can continue writing this plugin
+        // as a modern ECMA module, while still publishing this as a CommonJS
+        // library with a default export, as that's how ESLint expects plugins to look.
+        // @see https://github.com/evanw/esbuild/issues/1182#issuecomment-1011414271
+        js: 'module.exports = module.exports.default;',
+      };
+    },
   },
 ]);
